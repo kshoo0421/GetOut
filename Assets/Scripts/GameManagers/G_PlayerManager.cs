3386b41e7cd2122a28ccf18a59d088777047977e
kshoo0421 <shmd02@google.com>
2023-05-08 01:00:37 +0900

    Script Initialize
    +Add Pictures

---
+++ Assets/Scripts/GameManagers/G_PlayerManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class G_PlayerManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
